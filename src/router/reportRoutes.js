f25f8218d1429ae69c81f9765150bba8a9141903
Jeong Tae Gyeong <mcinsu401@naver.com>
2025-02-10 19:20:10 +0900

    initial commit

--- src/router/reportRoutes.js
+++ src/router/reportRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const reportController = require("../controllers/reportController"); 
 
 router.get("/", reportController.getReports); 
+router.get("/details", reportController.getReportDetails);
 
 module.exports = router;
